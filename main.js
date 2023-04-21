29dc6a6e33db61f237b2a8a1e49319e1040bae3c
omiras <oscar.miras@gmail.com>
2023-04-21 09:33:49 +0000

    complteado enunciado

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
-// Estos correos ya se han apuntado a la notificación
+// Estos correos ya se han apuntado a la notificación. Esto es para el ejercicio 2
 const registeredEmails = [
   "paquita@gmail.com",
   "rosa@gmail.com",
   "faustino@gmail.com",
 ];
+
+
